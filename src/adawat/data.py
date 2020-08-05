70e5de6a20b199e20954dabceef89b15928cdc2e
Rafid Al-Humaimidi <rafidka@gmail.com>
2020-08-04 20:39:49 -0700

    ListDataset: Separate transforms for features/targets
    ListDataset supported one transform acting on a tuple of
    (feature, target). This is not convenient since the transforms applied
    on features need not be the same as the ones applied to targets.

--- src/adawat/data.py
+++ src/adawat/data.py
@@ -7,7 +7,9 @@ class ListDataset(torch.utils.data.Dataset):
     """Converts a normal Python list into a PyTorch Dataset for use with
     DataLoaders."""
 
-    def __init__(self, features: List, targets: List, transform: Callable[[Any], torch.tensor] = None):
+    def __init__(self, features: List, targets: List,
+                 features_transform: Callable[[Any], torch.tensor] = None,
+                 targets_transform: Callable[[Any], torch.tensor] = None):
         if len(features) != len(targets):
             raise ValueError(f"X and y must be of the same length. Received " +
                              "{len(X)} for X and {len(y)} for y.")
@@ -15,10 +17,12 @@ class ListDataset(torch.utils.data.Dataset):
         self.targets = targets
         # save length in case it is expensive to calculate
         self.len = len(self.features)
-        self.transform = transform if transform is not None else lambda x: x
+        self.features_transform = features_transform if features_transform is not None else lambda x: x
+        self.targets_transform = targets_transform if targets_transform is not None else lambda x: x
 
     def __getitem__(self, idx: int) -> Any:
-        return self.transform((self.features[idx], self.targets[idx]))
+        return (self.features_transform(self.features[idx]),
+                self.targets_transform(self.targets[idx]))
 
     def __len__(self) -> int:
         return self.len
