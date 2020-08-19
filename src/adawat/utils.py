7972688fc603bad6b43434032f3fa920c18d81b0
Rafid Al-Humaimidi <rafidka@gmail.com>
2020-08-18 23:55:08 -0700

    [GeneratorWithLen] Accept a function that returns a generator
    Instead of just accepting a generator, the class can now also accept a
    function that returns a generator when called. One usage of this is to
    be able to iterate a function that uses the `yield` keyword more than
    once (I mean iterate more than once) by passing the function itself to
    this class.

--- src/adawat/utils.py
+++ src/adawat/utils.py
@@ -3,6 +3,10 @@ class GeneratorWithLen(object):
     With generators that doesn't have support for the `len` method but the
     length can be calculated in advance, e.g. a yield in a for-loop, this
     class adds support for the `len` method.
+
+    Keyword arguments:
+    gen -- The generator or a callable that returns a generator.
+    length -- The length of the iterable.
     """
 
     def __init__(self, gen, length):
@@ -13,4 +17,7 @@ class GeneratorWithLen(object):
         return self.length
 
     def __iter__(self):
-        return self.gen
+        if callable(self.gen):
+            return self.gen()
+        else:
+            return self.gen
