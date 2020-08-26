99a37d80535d6e7fa9d889d40fa780c23be64954
Rafid Al-Humaimidi <rafidka@gmail.com>
2020-08-25 23:13:28 -0700

    Implement S3Pickler
    This is similar to FilePickler, except that it uses S3 to store the
    state of objects instead of local files.

--- src/adawat/picklers.py
+++ src/adawat/picklers.py
@@ -1,10 +1,19 @@
 from abc import ABC, abstractmethod
+import logging
 import os
 import pickle
 import tempfile
 
 
+log = logging.getLogger(__name__)
+
+
 class ObjectNotFoundError(RuntimeError):
+    """
+    This exception is thrown be the `load` methods of picklers to indicate that
+    load failed. This is then used by the @stateful decorator to proceed with
+    the normal initialization of an object.;
+    """
     pass
 
 
@@ -24,12 +33,12 @@ class FilePickler(Pickler):
         filepath = os.path.join(tempfile.gettempdir(), filename)
         return filepath
 
-    def dump(self, obj_id, obj):
+    def dump(self, obj_id: str, obj):
         filepath = self._filepath(obj_id)
         with open(filepath, 'wb') as file:
             pickle.dump(obj, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-    def load(self, obj_id):
+    def load(self, obj_id: str):
         filepath = self._filepath(obj_id)
 
         if not os.path.isfile(filepath):
@@ -38,3 +47,42 @@ class FilePickler(Pickler):
 
         with open(filepath, 'rb') as file:
             return pickle.load(file)
+
+
+class S3Pickler(Pickler):
+    def __init__(self, client, bucket: str):
+        self.client = client
+        self.bucket = bucket
+
+    def dump(self, obj_id: str, obj):
+        obj_bytes = pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
+        try:
+            self.client.put_object(
+                Body=obj_bytes, Bucket=self.bucket, Key=obj_id)
+        except Exception as e:
+            # dump() methods silently fail so it doesn't impact execution, so
+            # we just log the exception for visibility.
+            log.exception(e)
+
+    def load(self, obj_id: str):
+        try:
+            response = self.client.get_object(Bucket=self.client, Key=obj_id)
+            obj_bytes = response['Body'].read()
+            return pickle.loads(obj_bytes)
+        except Exception as e:
+            raise ObjectNotFoundError() from e
+
+
+def set_lifecycle_policy(s3, bucket: str, expire_in_days: int):
+    s3.put_bucket_lifecycle(Bucket=bucket, LifecycleConfiguration={
+        'Rules': [
+            {
+                'ID': 'expiry-rule',
+                'Expiration': {
+                    'Days': expire_in_days,
+                },
+                'Prefix': '',
+                'Status': 'Enabled',
+            },
+        ]
+    })
