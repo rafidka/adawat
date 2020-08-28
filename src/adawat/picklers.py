01691b9c6101e10ff014ae5b6a42cd9af2abff8c
Rafid Al-Humaimidi <rafidka@gmail.com>
2020-08-27 19:39:39 -0700

    [S3Pickler] Fix load() bug + auto create bucket
    Discovered a bug with the `load()` method which caused it to fail
    always. Also, updated the class such that it automatically creates the
    bucket if it doesn't exist.

--- src/adawat/picklers.py
+++ src/adawat/picklers.py
@@ -53,12 +53,67 @@ class S3Pickler(Pickler):
     def __init__(self, client, bucket: str):
         self.client = client
         self.bucket = bucket
+        self.ready = False
+
+        self._create_bucket()
+        # delete after a week to reduce costs.
+        self._set_lifecycle_policy(expire_in_days=7)
+
+    def _bucket_exists(self) -> bool:
+        try:
+            self.client.head_bucket(Bucket=self.bucket)
+            return True
+        except self.client.exceptions.ClientError:
+            # The bucket does not exist or you have no access.
+            return False
+
+    def _create_bucket(self):
+        if not self._bucket_exists():
+            try:
+                log.info(f"Bucket {self.bucket} doesn't exist. Creating it...")
+                self.client.create_bucket(Bucket=self.bucket)
+                self.ready = True
+                log.info(f"Bucket {self.bucket} created.")
+            except Exception as e:
+                log.exception(e)
+                # The bucket doesn't exist and we couldn't create it. Mark the
+                # pickler as not ready for use.
+                self.ready = False
+        else:
+            log.debug(
+                f"Bucket {self.bucket} already exists. No need to create it.")
+
+    def _set_lifecycle_policy(self, expire_in_days: int):
+        try:
+            log.info(f'Set lifecycle policy for bucket {self.bucket} ' +
+                     f'with expiration of {expire_in_days} days')
+            self.client.put_bucket_lifecycle(Bucket=self.bucket, LifecycleConfiguration={
+                'Rules': [
+                    {
+                        'ID': 'expiry-rule',
+                        'Expiration': {
+                            'Days': expire_in_days,
+                        },
+                        'Prefix': '',
+                        'Status': 'Enabled',
+                    },
+                ]
+            })
+        except Exception as e:
+            log.fatal(f"Failed to set life")
+            log.exception(e)
 
     def dump(self, obj_id: str, obj):
         obj_bytes = pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
         try:
+            log.debug(
+                f"Saving object with id {obj_id} to s3://{self.bucket}/{obj_id} .")
+
             self.client.put_object(
                 Body=obj_bytes, Bucket=self.bucket, Key=obj_id)
+
+            log.debug(
+                f"Saved object with id {obj_id} to s3://{self.bucket}/{obj_id} .")
         except Exception as e:
             # dump() methods silently fail so it doesn't impact execution, so
             # we just log the exception for visibility.
@@ -66,23 +121,17 @@ class S3Pickler(Pickler):
 
     def load(self, obj_id: str):
         try:
-            response = self.client.get_object(Bucket=self.client, Key=obj_id)
+            log.debug(
+                f"Loading object with id {obj_id} from s3://{self.bucket}/{obj_id} .")
+
+            response = self.client.get_object(Bucket=self.bucket, Key=obj_id)
             obj_bytes = response['Body'].read()
-            return pickle.loads(obj_bytes)
+            obj = pickle.loads(obj_bytes)
+
+            log.debug(
+                f"Loaded object with id {obj_id} from s3://{self.bucket}/{obj_id} .")
+
+            return obj
         except Exception as e:
+            log.exception(e)
             raise ObjectNotFoundError() from e
-
-
-def set_lifecycle_policy(s3, bucket: str, expire_in_days: int):
-    s3.put_bucket_lifecycle(Bucket=bucket, LifecycleConfiguration={
-        'Rules': [
-            {
-                'ID': 'expiry-rule',
-                'Expiration': {
-                    'Days': expire_in_days,
-                },
-                'Prefix': '',
-                'Status': 'Enabled',
-            },
-        ]
-    })
